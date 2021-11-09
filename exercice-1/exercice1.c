d92e3260a7193d708f95b14c517728c98fe34ddd
Hamza Dabaghi <hamza.dabaghi@gmail.com>
2021-11-09 03:07:35 +0100

    exercice 1 works , still to do the same for struts variables

--- exercice-1/exercice1.c
+++ exercice-1/exercice1.c
@@ -4,7 +4,6 @@
 
 #include "exercice1.h"
 
-
 /*  2- Définir une fonction creer permettant de créer un tableau générique de n éléments	 */
 
 T creer(int n,int taille_element) {
@@ -12,7 +11,7 @@ T creer(int n,int taille_element) {
 	T tab = (T) malloc(sizeof(struct tableau));
 	tab->nb_element = n;
 	tab->taille_reel = 0;
-	tab->tab = (void**)malloc(tab->nb_element * taille_element ); // * sizeof(void*) instead of taille_element
+	tab->tab = (void**)malloc(tab->nb_element * taille_element );
 	return tab;
 }
 
@@ -43,11 +42,8 @@ T  aleatoire(int n,int taille_element,void (*affic)(void *),void* (*aleat)(),voi
 	tab->detru = detru;
 
 	for(i=0 ; i < tab->nb_element; i++){
-
-					void * element;
-
+					void* element = tab->aleat();
 					tab->tab[i] =tab->aleat();
-
 					tab->taille_reel+=1;
 
 
@@ -61,33 +57,37 @@ T  aleatoire(int n,int taille_element,void (*affic)(void *),void* (*aleat)(),voi
 
 /*  5- Définir une fonction detruire_tout permettant de libérer correctement la mémoire  */
 
-/*
+
 void detruire_tout(T tableau) {
 
+	tableau->nb_element=0;
 	tableau->taille_reel=0;
-	free(tableau->tab);
-    free(tableau);
+	tableau->detru(tableau);
+
+
 
 }
-*/
+
 
 /*  6- Définir une fonction trier permettant de trier les éléments du tableau générique  */
 
-/*
+
 void trier(T tableau ,int taille) {
 
-	int i , j ;
-	void* c;
+	int i, j, c;
+	for(i=0;i<taille-1;i++){
+			for(j=i+1;j<taille;j++){
+		    	 if ( *(int *)tableau->tab[i] > *(int *) tableau->tab[j] ) {
+		            void* element = tableau->tab[i];
+		            tableau->tab[i] = tableau->tab[j];
+		            tableau->tab[j] = element ;
+		            
+		        }
+	}
+       
 
-	for(i=0;i<taille-1;i++)
 
-    for(j=i+1;j<taille;j++)
-        if ( tableau->tab[i] > tableau->tab[j] ) {
-            c = &tableau->tab[i];
-            tableau->tab[i] = tableau->tab[j];
-            tableau->tab[j] = (int*)c;
-        }
-
-
-}*/
+	}
+    
+}
 #endif
