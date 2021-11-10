2dfa017354a78f2396490f5e504022edd5287077
Hamza Dabaghi <hamza.dabaghi@gmail.com>
2021-11-10 18:15:45 +0100

    fix realloc problem

--- exercice-2/exercice2.c
+++ exercice-2/exercice2.c
@@ -1,6 +1,7 @@
 #ifndef TAB_HAs
 #define	TAB_HAs
 #include "exercice2.h"
+#include<stdlib.h>
 
 /*  2- Définir une fonction creer permettant de créer un tableau générique de n éléments	 */
 
@@ -87,7 +88,7 @@ void push(T_var tableau, void* item, int taille_element){
 		tableau->nb_element = tableau->nb_element * 2;
 		int newSize = tableau->nb_element * taille_element;
 		printf("\n**Before realloc**\n");
-		tableau->tab = (void**)realloc(tableau->tab, newSize);
+		tableau->tab = realloc(tableau->tab, newSize);
 		printf("\n**After realloc**\n");
 		tableau->tab[tableau->taille_reel] = (int*)t;
 		tableau->taille_reel++;
@@ -96,10 +97,10 @@ void push(T_var tableau, void* item, int taille_element){
 	
 }
 
-
+/*
 void* pop(T_var tableau, int taille_element){
 	
 	return tableau->tab[tableau->taille_reel];
 }
-
+*/
 #endif
